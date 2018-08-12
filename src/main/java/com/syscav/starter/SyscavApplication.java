e686ce24d1a92ea434b139aee31f5bae8c25e564
Sosthene Nouebissi <sosthenegolden@gmail.com>
2018-08-12 01:17:42 +0100

    Prepare RestController and Controller

---
+++ src/main/java/com/syscav/starter/SyscavApplication.java
@@ -0,0 +1,12 @@
+package com.syscav.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyscavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyscavApplication.class, args);
+	}
+}
